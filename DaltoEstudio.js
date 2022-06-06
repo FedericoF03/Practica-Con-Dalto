267d12a063b4109624134e565cb3a05be6f82336
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-06 03:11:47 -0300

    Borre datos innecesarios

--- DaltoEstudio.js
+++ DaltoEstudio.js
@@ -1,1 +1,12 @@
-console.logHola mundo
+let object1 = {
+    nombre: "la master race papi",
+    procesador: "no tengo xD"
+};
+
+let nombre = object1["nombre"]
+let procesador = object1["procesador"]
+
+
+frase = `el nombre de mi pc es: ${nombre}
+el procesador es: ${procesador}
+pepe`
