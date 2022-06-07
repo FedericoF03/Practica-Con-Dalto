5dac479a241b98752fe1b901227afdd901b1053c
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-06 22:49:55 -0300

    Esta comiteado tdo

--- DaltoEstudio.js
+++ DaltoEstudio.js
@@ -1,12 +1,51 @@
-let object1 = {
-    nombre: "la master race papi",
-    procesador: "no tengo xD"
-};
+// class Animal {
+//     constructor(especie, edad, color){
+//         this.especie = especie;
+//         this.edad = edad;
+//         this.color = color;
+//         this.info = `Soy ${this.especie}, tengo ${this.edad} años
+//         y soy de color ${this.color}`;
+//     }
+//     verInfo() {
+//         console.log(this.info)
+//     }
+// }
 
-let nombre = object1["nombre"]
-let procesador = object1["procesador"]
+
+// class Perro extends Animal {
+//     constructor(especie, edad, color, raza) {
+//             super(especie,edad,color);
+//             this.raza = null;
+//     }
+//     set setRaza(newName){
+//         this.raza = newName
+//     }
+
+//     get getRaza() {
+//         return this.raza
+//     }
+// }
 
 
-frase = `el nombre de mi pc es: ${nombre}
-el procesador es: ${procesador}
-pepe`
+
+
+
+
+
+// let perro = new Perro("perro", 5, "negro", "RottWeiller")
+// let gato = new Animal("Gato", "3 meses", "negro")
+// let pajaro = new Animal("pajaro", 3, "fusia")
+
+// perro.setRaza = "Pedrito" 
+// console.log(perro.getRaza)
+
+
+// materias = {
+//     fisica: ["pruebo"],
+//     quimica: ["sigoprogabdo"],
+//     pepe: ["sjdskjd", "sjdskjdksj", "gkbgihnbgk"]
+// }
+
+// console.log(materias["pepe"][1])
+
+
