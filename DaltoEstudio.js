3565a8b9954d8c68ba63555dd09215f6bfff9f9e
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-07 17:31:45 -0300

    Se agrego codigo a .js, css/Style tiene estilos, index.html se relleno

--- DaltoEstudio.js
+++ DaltoEstudio.js
@@ -1,51 +1,16 @@
-// class Animal {
-//     constructor(especie, edad, color){
-//         this.especie = especie;
-//         this.edad = edad;
-//         this.color = color;
-//         this.info = `Soy ${this.especie}, tengo ${this.edad} años
-//         y soy de color ${this.color}`;
-//     }
-//     verInfo() {
-//         console.log(this.info)
-//     }
-// }
+'use strict';
 
+let cash = document.getElementById("amountInput")
+let button = document.getElementById("amountButton")
+button.addEventListener("click", calculate)
 
-// class Perro extends Animal {
-//     constructor(especie, edad, color, raza) {
-//             super(especie,edad,color);
-//             this.raza = null;
-//     }
-//     set setRaza(newName){
-//         this.raza = newName
-//     }
+let calculate = () => {
+    console.log(cash.value)
+    cash[1] = parseFloat(cash[1])
 
-//     get getRaza() {
-//         return this.raza
-//     }
-// }
+    if(ownerCash > 0.6 ){
 
-
-
-
-
-
-
-// let perro = new Perro("perro", 5, "negro", "RottWeiller")
-// let gato = new Animal("Gato", "3 meses", "negro")
-// let pajaro = new Animal("pajaro", 3, "fusia")
-
-// perro.setRaza = "Pedrito" 
-// console.log(perro.getRaza)
-
-
-// materias = {
-//     fisica: ["pruebo"],
-//     quimica: ["sigoprogabdo"],
-//     pepe: ["sjdskjd", "sjdskjdksj", "gkbgihnbgk"]
-// }
-
-// console.log(materias["pepe"][1])
+    }
+}
 
 
