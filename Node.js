82f5bf0d9659097dc1eba444caca6243b5c559a9
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-08 18:52:14 -0300

    Practique y agregue ejemplos

--- Node.js
+++ Node.js
@@ -2,13 +2,41 @@
 
 const parrafo = document.getElementById("id__prueba")
 const probando = "hola"
-let functionPrueba = (parametro) => console.log(`${parametro} mundo!`)
 const input = document.getElementsByTagName("input")
-console.log(input[0])
-input[0].setAttribute("type", "text" )
-const see = parrafo.getAttribute("value")
-console.log(see)
+let probando1 = parrafo.outerHTML
+const claro = document.createDocumentFragment()
+const contenedor = document.querySelector(".conteiner")
 
+ let i = parrafo.nextElementSibling
+ console.log(i)
+for(let i = 0; i < 20; i++) {
+    const item1 = document.createElement("DIV")
+    item1.innerHTML = "probando funcionalidades"
+    claro.appendChild(item1)
+}
+contenedor.appendChild(claro)
+
+let pruebaa = contenedor.children
+console.log(pruebaa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let functionPrueba = (parametro) => console.log(`${parametro} mundo!`)
 for (let index = 0; index < parrafo.length; index++) {
     parrafo[0].addEventListener("click", () => functionPrueba(probando))
 }
